d41f657f70cfe69f52688a11cc9e4a1bd511281d
Maciej Banas <banasmaciek@gmail.com>
2025-06-11 22:47:49 +0200

    Add some assets.

--- inst/examples/bear_game.R
+++ inst/examples/bear_game.R
@@ -5,4 +5,16 @@ game <- PhaserGame$new(width = 1600, height = 800)
 ui <- game$ui()
 
 server <- function(input, output, session) {
+  game$add_player_sprite(
+    name = "bear",
+    url = "assets/bear_game/player_sprites/bear_idle.png",
+    x = 100,
+    y = 100,
+    frameWidth = 100,
+    frameHeight = 100,
+    frameCount = 10,
+    frameRate = 4
+  )
 }
+
+shinyApp(ui, server)
