7a29ead2bab0b6ffdced664518e3356e0d3f7663
Maciej Banas <banasmaciek@gmail.com>
2025-06-12 20:08:10 +0200

    Complete documentation.

--- R/PhaserGame.R
+++ R/PhaserGame.R
@@ -1,14 +1,21 @@
-#' @title PhaserGame class object
+#' @title PhaserGame
+#' @description R6 class to create and manage a Phaser game within a Shiny application.
+#' Provides methods for adding sprites, animations, images, backgrounds, controls, and collision handling.
+#'
 #' @export
 PhaserGame <- R6::R6Class(
   "PhaserGame",
   public = list(
-    #' @field id ID of the Game container.
+    #' @field id Character. ID of the Game container. Used as the HTML element ID where the game canvas will be rendered.
     id = NULL,
-    #' Create PhaserGame object with configuration
-    #' @param id ID of the Game container.
-    #' @param width Width of canvas element in pixels.
-    #' @param height Height of canvas element in pixels.
+
+    #' @description Create a PhaserGame object with the given configuration.
+    #' @param id Character. ID of the Game container (defaults to "phaser_game").
+    #' @param width Numeric. Width of the Phaser canvas in pixels (defaults to 800).
+    #' @param height Numeric. Height of the Phaser canvas in pixels (defaults to 600).
+    #' @return A new PhaserGame object.
+    #' @examples
+    #' game <- PhaserGame$new(id = "my_game", width = 1024, height = 768)
     initialize = function(id = "phaser_game",
                           width = 800,
                           height = 600) {
@@ -18,7 +25,13 @@ PhaserGame <- R6::R6Class(
         height = height
       )
     },
-    #' @description Load dependencies and initialize Phaser.Game
+
+    #' @description Load dependencies and initialize the Phaser game in the UI.
+    #' @return HTML tag list containing dependencies and initialization script.
+    #' @examples
+    #' ui <- fluidPage(
+    #'   game$ui()
+    #' )
     ui = function() {
       shiny::addResourcePath("assets", system.file("assets", package = "phaserR"))
       htmltools::tagList(
@@ -37,59 +50,66 @@ PhaserGame <- R6::R6Class(
         )
       )
     },
-    #' @param x X position of player sprite.
-    #' @param y Y position of player sprite.
-    #' @param frameWidth Should be adjusted to sprite resolution.
-    #' @param frameHeight Should be adjusted to sprite resolution.
-    #' @param frameCount Number of frames in sprite.
-    #' @param frameRate Speed of rendering frames (number of frames per second).
+
+    #' @description Add a player sprite to the scene as an animated spritesheet.
+    #' @param name Character. Unique key for the sprite.
+    #' @param url Character. URL or relative path to the spritesheet image.
+    #' @param x Numeric. X-coordinate in pixels.
+    #' @param y Numeric. Y-coordinate in pixels.
+    #' @param frameWidth Numeric. Width of each frame in the spritesheet.
+    #' @param frameHeight Numeric. Height of each frame in the spritesheet.
+    #' @param frameCount Numeric. Total number of frames.
+    #' @param frameRate Numeric. Frames per second for the animation.
+    #' @param session Shiny session object (default: shiny::getDefaultReactiveDomain()).
+    #' @return Invisible; sends a custom message to the client.
     add_player_sprite = function(name, url, x, y, frameWidth, frameHeight, frameCount, frameRate,
                                  session = shiny::getDefaultReactiveDomain()) {
       js <- sprintf("addPlayerSprite('%s', '%s', %d, %d, %d, %d, %d, %d);",
                     name, url, x, y, frameWidth, frameHeight, frameCount, frameRate)
       session$sendCustomMessage("phaser", list(js = js))
     },
-    #' @description Load a custom animation for any sprite.
-    #'   `suffix` should match the suffix you want (e.g. "move_left", "move_right", "move").
-    #' @param name character: base key you used in add_player_sprite() or add_enemy_sprite().
-    #' @param suffix character: one of "move_left", "move_right", "move", etc.
-    #' @param url character: path to the spritesheet image.
-    #' @param frameWidth numeric: width of each frame.
-    #' @param frameHeight numeric: height of each frame.
-    #' @param frameCount numeric: number of frames in the spritesheet.
-    #' @param frameRate numeric: frames per second to play this animation.
+
+    #' @description Load a custom animation for any sprite previously added.
+    #' @param name Character. Base key used in add_player_sprite or add_enemy_sprite.
+    #' @param suffix Character. Identifier for this animation (e.g. "move_left").
+    #' @param url Character. URL or path to the spritesheet.
+    #' @param frameWidth Numeric. Width of each frame.
+    #' @param frameHeight Numeric. Height of each frame.
+    #' @param frameCount Numeric. Number of frames in the spritesheet.
+    #' @param frameRate Numeric. Frames per second for playback.
+    #' @param session Shiny session object (default: shiny::getDefaultReactiveDomain()).
+    #' @return Invisible; sends a custom message to the client.
     add_sprite_animation = function(name, suffix, url,
                                     frameWidth, frameHeight,
                                     frameCount, frameRate,
                                     session = shiny::getDefaultReactiveDomain()) {
-      # Build a JS call to addSpriteAnimation(name, suffix, url, frameWidth, frameHeight, frameCount, frameRate)
       js <- sprintf(
         "addSpriteAnimation('%s','%s','%s',%d,%d,%d,%d);",
         name, suffix, url, frameWidth, frameHeight, frameCount, frameRate
       )
       session$sendCustomMessage("phaser", list(js = js))
     },
+
     #' @description Adds a static image to the Phaser scene.
-    #'   The image will be placed at the specified (x, y) pixel coordinates.
-    #'
-    #' @param imageName character: a unique key to reference this image in Phaser.
-    #' @param imageUrl character: URL or relative path to the image file.
-    #' @param x numeric: horizontal position in pixels where the image will be placed.
-    #' @param y numeric: vertical position in pixels where the image will be placed.
+    #' @param imageName Character. Unique key to reference this image.
+    #' @param imageUrl Character. URL or path to the image file.
+    #' @param x Numeric. X-coordinate in pixels.
+    #' @param y Numeric. Y-coordinate in pixels.
     #' @param session Shiny session object (default: shiny::getDefaultReactiveDomain()).
+    #' @return Invisible; sends a custom message to the client.
     add_image = function(imageName, imageUrl, x, y, session = shiny::getDefaultReactiveDomain()) {
       js <- sprintf("addImage('%s', '%s', %d, %d);", imageName, imageUrl, x, y)
       session$sendCustomMessage("phaser", list(js = js))
     },
-    #' Add a background/tilemap layer from Tiled JSON + tileset image.
-    #'
-    #' @param mapKey A short string key (used internally in Phaser) for your tilemap JSON.
-    #' @param mapUrl URL (relative to www/assets/) of the `.json` exported from Tiled.
-    #' @param tilesetNames The exact names of the tileset as it appears inside Tiled (must match the Tiled "Tileset" name).
-    #' @param tilesetUrls URLs (relative to www/assets/) of the `PNG` or `JPG` used by your Tiled map.
-    #' @param layerName The name of the layer inside the Tiled map (e.g. "Ground") that you want to render.
-    #' @param session Shiny session.
-    # Inside your PhaserGame R6:
+
+    #' @description Add a background (tilemap) layer from Tiled JSON + tileset image(s).
+    #' @param mapKey Character. Key for the tilemap JSON.
+    #' @param mapUrl Character. URL of the Tiled JSON file (relative to www/assets/).
+    #' @param tilesetUrls Character vector. URLs of tileset image files.
+    #' @param tilesetNames Character vector. Names of tilesets as defined in Tiled.
+    #' @param layerName Character. Name of the layer to render from Tiled.
+    #' @param session Shiny session object (default: shiny::getDefaultReactiveDomain()).
+    #' @return Invisible; sends a custom message to the client.
     add_map = function(mapKey,
                        mapUrl,
                        tilesetUrls,
@@ -106,36 +126,44 @@ PhaserGame <- R6::R6Class(
       )
       session$sendCustomMessage("phaser", list(js = js))
     },
-    #' Enable movement controls (arrow keys) for a player
-    #' @param name Name of the player sprite (as given in add_player_sprite)
-    #' @param speed Movement speed in pixels/sec (default: 200)
+
+    #' @description Enable movement controls (arrow keys) for a player sprite.
+    #' @param name Character. Name of the player sprite (as added via add_player_sprite).
+    #' @param directions Character vector. Directions to enable (defaults to c("left","right","down","up")).
+    #' @param speed Numeric. Movement speed in pixels/second (default: 200).
+    #' @param session Shiny session object (default: shiny::getDefaultReactiveDomain()).
     add_player_controls = function(name, directions = c("left", "right", "down", "up"),
                                    speed = 200,
                                    session = shiny::getDefaultReactiveDomain()) {
       js_dirs <- jsonlite::toJSON(directions, auto_unbox = TRUE)
-      js <- sprintf("addPlayerControls('%s', '%s', %d);", name, js_dirs, speed)
+      js <- sprintf("addPlayerControls('%s', %s, %d);", name, js_dirs, speed)
       session$sendCustomMessage("phaser", list(js = js))
     },
-    #' Enable terrain collision for a player
-    #' @param name Name of the player sprite (as given in add_player_sprite)
+
+    #' @description Enable terrain collision for a player sprite.
+    #' @param name Character. Name of the player sprite (as added via add_player_sprite).
+    #' @param session Shiny session object (default: shiny::getDefaultReactiveDomain()).
     enable_terrain_collision = function(name, session = shiny::getDefaultReactiveDomain()) {
       js <- sprintf("addPlayerTerrainCollider('%s');", name)
       session$sendCustomMessage("phaser", list(js = js))
     },
-    #' @description Adds static sprite to scene.
-    #' @param name A character, unique name of the sprite.
-    #' @param url A character, URL or path to the image file.
-    #' @param x A numeric - horizontal position (in pixels).
-    #' @param y A numeric - vertical position (in pixels.)
+
+    #' @description Adds a static sprite to the scene (non-animated).
+    #' @param name Character. Unique name of the sprite.
+    #' @param url Character. URL or path to the image file.
+    #' @param x Numeric. X-coordinate in pixels.
+    #' @param y Numeric. Y-coordinate in pixels.
+    #' @param session Shiny session object (default: shiny::getDefaultReactiveDomain()).
     add_static_sprite = function(name, url, x, y, session = shiny::getDefaultReactiveDomain()) {
       js <- sprintf("addStaticSprite('%s','%s',%s,%s);",
-                         name, url, x, y)
+                    name, url, x, y)
       session$sendCustomMessage("phaser", list(js = js))
     },
 
-    #' @description Adds a collider between two objects
-    #' @param sprite_one_name A character, name of the first object.
-    #' @param sprite_two_name A character, name of the second object.
+    #' @description Adds a collider between two game objects.
+    #' @param object_one_name Character. Name of the first object.
+    #' @param object_two_name Character. Name of the second object.
+    #' @param session Shiny session object (default: shiny::getDefaultReactiveDomain()).
     add_collider = function(object_one_name, object_two_name, session = shiny::getDefaultReactiveDomain()) {
       js <- sprintf("addCollider('%s','%s');",
                     object_one_name, object_two_name)
@@ -143,9 +171,15 @@ PhaserGame <- R6::R6Class(
     },
 
     #' @description Load a base spritesheet and create an "idle" animation.
-    #' @param url A character, URL or path to the image file.
-    #' @param x A numeric - horizontal position (in pixels).
-    #' @param y A numeric - vertical position (in pixels.)
+    #' @param name Character. Unique key for the sprite and its idle animation.
+    #' @param url Character. URL or path to the spritesheet image.
+    #' @param x Numeric. X-coordinate in pixels.
+    #' @param y Numeric. Y-coordinate in pixels.
+    #' @param frameWidth Numeric. Width of each frame.
+    #' @param frameHeight Numeric. Height of each frame.
+    #' @param frameCount Numeric. Number of frames in the spritesheet.
+    #' @param frameRate Numeric. Frames per second for the idle animation.
+    #' @param session Shiny session object (default: shiny::getDefaultReactiveDomain()).
     add_sprite = function(name, url,
                           x, y,
                           frameWidth, frameHeight,
@@ -158,13 +192,14 @@ PhaserGame <- R6::R6Class(
       session$sendCustomMessage("phaser", list(js = js))
     },
 
-    #' @description Put all existing sprites of this type into motion along (dirX, dirY)
-    #'   at the given speed, and stop them once they’ve traveled `distance` pixels.
-    #' @param type A character: the key used in add_sprite()
-    #' @param dirX A numeric: horizontal direction (–1 = left, +1 = right, 0 = no horizontal)
-    #' @param dirY A numeric: vertical direction (–1 = up,   +1 = down,  0 = no vertical)
-    #' @param speed A numeric: number of px/sec to move in that (dirX,dirY) direction
-    #' @param distance  A numeric: how many pixels (Euclidean) to travel before stopping
+    #' @description Move all sprites of a given type along a vector for a set distance.
+    #' @param type Character. Key used in add_sprite().
+    #' @param dirX Numeric. Horizontal direction (-1 = left, +1 = right, 0 = none).
+    #' @param dirY Numeric. Vertical direction (-1 = up, +1 = down, 0 = none).
+    #' @param speed Numeric. Speed in pixels/second.
+    #' @param distance Numeric. Distance in pixels to travel before stopping.
+    #' @param lag Numeric. Optional delay before sending the command (defaults to distance/speed).
+    #' @param session Shiny session object (default: shiny::getDefaultReactiveDomain()).
     set_sprite_in_motion = function(type,
                                     dirX,
                                     dirY,
@@ -179,8 +214,8 @@ PhaserGame <- R6::R6Class(
       )
       session$sendCustomMessage("phaser", list(js = js))
     }
-
   ),
+
   private = list(
     config = list()
   )
