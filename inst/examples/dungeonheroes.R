ddb7d8daa48c84ad1073aa4b6ef44fed2f22065c
Maciej Banas <banasmaciek@gmail.com>
2026-08-04 14:30:52 +0200

    Update assets and init file.

--- inst/examples/dungeonheroes.R
+++ inst/examples/dungeonheroes.R
@@ -1,8 +1,2 @@
-# Compatibility launcher for users who previously sourced this file directly.
-# Prefer the development namespace loaded by devtools::load_all(); fall back to
-# the installed package when this file is run outside the source repository.
-if (exists("run_dungeonheroes", mode = "function", inherits = TRUE)) {
-  run_dungeonheroes()
-} else {
-  shinyphaser::run_dungeonheroes()
-}
+devtools::load_all()
+run_dungeonheroes()
